1bc1b519a2630b9caa67b342729d7ade794c32aa
angelwamou <022100304G@uandina.edu.pe>
2025-02-26 12:53:55 -0500

    Add files via upload

---
+++ Con_Fe.py
@@ -0,0 +1,20 @@
+Kfrutas = int(input(""))
+resultado= (Kfrutas*2)/3
+
+entero= resultado % 10
+decimal= resultado // 10
+
+if decimal <= 0.29:
+    decimal=0
+if decimal <= 0.7:
+    decimal=0.5
+else:
+    decimal=1
+print(f"{entero}, {decimal}")
+
+
+
+
+
+
+
